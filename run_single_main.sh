e49573289052633108a436f570a6d99e22124c94
lurovi <luigirovito2@gmail.com>
2024-12-12 17:44:11 +0100

    Version 1.2.0

--- run_single_main.sh
+++ run_single_main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 start=$(date +%s)
-python3 main.py --method ${1} --seed ${2} --dataset ${3} --test_size ${4} --config ${5} --run_id ${6} --verbose ${7}
+python3 main.py --method ${1} --seed ${2} --dataset ${3} --normalize ${4} --test_size ${5} --config ${6} --run_id ${7} --verbose ${8}
 end=$(date +%s)
 
